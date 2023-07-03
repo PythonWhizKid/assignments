75e4745a4db111aabc01cfdea587fcf97b24b092
Python Whiz Kid <138038178+PythonWhizKid@users.noreply.github.com>
2023-07-03 14:03:18 +0530

    Update solution1.py

--- assignment2/solution1.py
+++ assignment2/solution1.py
@@ -3,7 +3,7 @@ def prime_check(n):
     if n < 2:
         print("The given number is not a prime number")
         return
-    for i in range(2, int(n**0.5)+1):
+    for i in range(2, int(n**0.5)+1): #factors bigger than square root will eventually be multiplied b factors smaller than it as 36 = 12*3
         if n % i == 0:
             print("The given number is not a prime number")
             return

6ed540170ca6cb844c2a71be1163c7014a93bf51
PythonWhizKid <138038178+PythonWhizKid@users.noreply.github.com>
2023-07-03 12:12:55 +0530

    Create solution2.py

---
+++ assignment1/solution2.py
@@ -0,0 +1,15 @@
+#Assignment 2
+#Temperature converter
+from time import sleep
+print("xxxxxxxxxxxxxxxxxxxxx")
+sleep(0.8)
+print("xxxxxxxxxxxxxxxxxxxxx")
+sleep(0.8)
+C = float(input("Enter temperature in degree Celsius "))
+sleep(0.5)
+print("processing...")
+sleep(0.8)
+print("xxxxxxxxxxxxxxxxxxxxx")
+sleep(0.8)
+F = (C*9/5)+32
+print("Temp in Fahrenheit is: %s" % F)

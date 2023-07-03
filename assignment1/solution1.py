329af121da060512ddab9054230b9660ab99c9d5
PythonWhizKid <138038178+PythonWhizKid@users.noreply.github.com>
2023-07-03 12:12:19 +0530

    Create solution1.py

---
+++ assignment1/solution1.py
@@ -0,0 +1,15 @@
+#Assignment 1
+from time import sleep
+print("xxxxxxxxxxxxxxxxxxxxx")
+sleep(0.5)
+print("xxxxxxxxxxxxxxxxxxxxx")
+sleep(0.8)
+A = float(input("Enter first number "))
+B = float(input("Enter number to add "))
+R = A + B
+sleep(1)
+print("xxxxxxxxxxxxxxxxxxxxx")
+sleep(0.8)
+print("Answer: %s" % R)
+sleep(0.8)
+print("xxxxxxxxxxxxxxxxxxxxx")
